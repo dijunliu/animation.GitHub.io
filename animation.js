6536b230a7cce36081e0a0e140b4a1e5dbdcb457
liudijun <362172145@qq.com>
2018-11-17 13:09:32 +0800

    null

--- animation.js
+++ animation.js
@@ -1,26 +1,59 @@
 window.onload = function () {
-    tools.init();
+    canvas =  document.getElementById('animatecanvas');
+    context = canvas.getContext('2d');
+    animate.start();
 }
 
 animate = {
     start:function () {
-        var canvas =  document.getElementById('animatecanvas');
-        var context = canvas.getContext('2d');
+
         mouse = utils.capturemouse(canvas);
         utils.capturekey();
-        var arrow = new Arrow();
+        angle =0;
 
+        var arrow = new Arrow();
         arrow.x = canvas.width/2;
         arrow.y = canvas.height/2;
+        // (function drawFrame(){
+        //     window.requestAnimationFrame(drawFrame);
+        //     var dx = mouse.x - arrow.x,
+        //         dy = mouse.y - arrow.y;
+        //     context.clearRect(0,0,canvas.width,canvas.height);
+        //     arrow.rotation = Math.atan2(dy,dx);
+        //     arrow.draw(context);
+        //
+        // }());
+
 
         (function drawFrame(){
-            window.requestAnimationFrame(drawFrame);
-            var dx = mouse.x - arrow.x,
-                dy = mouse.y - arrow.y;
+            //window.requestAnimationFrame(drawFrame);
+            context.beginPath();
+            context.moveTo(angle,Math.sin(angle/10)*10+canvas.height/2);
+            for(var i = 0;i<Math.PI*80;i++){
+                context.lineTo(angle+i,Math.sin((angle+i)/10)*100+canvas.height/2);
+            }
+            context.stroke();
+            angle ++;
             context.clearRect(0,0,canvas.width,canvas.height);
-            arrow.rotation = Math.atan2(dy,dx);
-            arrow.draw(context);
-
         }());
+        // context.lineWidth = '3';
+        // function drawFrame(){
+        //     window.setTimeout(drawFrame,10);
+        //     context.clearRect(0,0,canvas.width,canvas.height);
+        //     context.beginPath();
+        //     context.moveTo(0,Math.sin(angle/10)*10+canvas.height/2);
+        //     for(var i = 0;i<canvas.width;i++){
+        //         context.lineTo(i,Math.sin((angle+i)/10)*100+canvas.height/2);
+        //     }
+        //     context.stroke();
+        //     angle ++;
+        //
+        // }
+        // drawFrame();
+    },
+    drawcircle(x,y,r){
+        context.beginPath();
+        context.arc(x,y,r,0,Math.PI*2,false);
+        context.fill();
     }
 }
